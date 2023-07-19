0831a394affacfd0f8d058cce18be1a4956384f4
Kaorimiao <2734427997@qq.com>
2023-07-19 21:48:22 +0800

    试试增加日语选项

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,13 +8,18 @@ export default defineUserConfig({
     "/": {
       lang: "en-US",
       title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
+      description: "A blog demo for Kaori",
     },
     "/zh/": {
       lang: "zh-CN",
       title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+      description: "Kaori的博客演示",
     },
+    "/ja/":{
+      lang:"ja-JP",
+      title:"ブログプレゼンです",
+      description:"Kaoriのブログプレゼンテーションです",
+    }
   },
 
   theme,
