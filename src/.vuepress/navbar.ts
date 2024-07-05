7b297c19bc3abb4b8ae2909185bd4c1fe0467e55
Kaorimiao <2734427997@qq.com>
2024-07-05 23:56:53 +0800

    first commit

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -9,18 +9,13 @@ export const zhNavbar = navbar([
     children: [
       { text: "代码笔记", link: "", icon: "code", activeMatch: "^/code/$" },
       {
-        text: "Go相关",
+        text: "C++相关",
         icon: "code",
         children: [
-          { text: "Go", link: "go/", icon: "golang1", activeMatch: "^/go/$" },
-          { text: "Gin", link: "gin/", icon: "houduankaifa", activeMatch: "^/gin/$" },
-          { text: "gRPC", link: "grpc/", icon: "golang", activeMatch: "^/gRPC/$" },
+          
         ],
-      },
-      { text: "MongoDB", link: "mongodb/", icon: "mongodb", activeMatch: "^/mongodb/$" },
-
+      },    
       { text: "项目", link: "project/", icon: "xiangmu", activeMatch: "^/project/$" },
-      { text: "博客部署", link: "vuepress/", icon: "blog", activeMatch: "^/vuepress/$" },
       { text: "其他", link: "other/", icon: "other1", activeMatch: "^/other/$" },
     ]
   },
@@ -29,20 +24,10 @@ export const zhNavbar = navbar([
     icon: "notepad",
     prefix: "/note/",
     children: [
-      { text: "随笔", link: "", icon: "notepad", activeMatch: "^/note/$" },
-      { text: "҉中҉二҉病҉", link: "me/", icon: "Super-Mario", activeMatch: "^/me/$" },
       { text: "动画", link: "anime/", icon: "a-zujiantianchong_huaban1fuben7", activeMatch: "^/anime/$" },
       { text: "游戏", link: "game/", icon: "game", activeMatch: "^/game/$" },
 
-      {
-        text: "借物表",
-        icon: "ziyuanjieyong",
-        prefix: "borrow/",
-        children: [
-          { text: "图片引用", link: "picture", icon: "picture-filling"},
-          { text: "框架引用", link: "program", icon: "code"},
-        ],
-      },
+      
     ],
   },
 ]);
